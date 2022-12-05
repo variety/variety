98a665eba50c4dfaf6cb8f67b3db9956e8e0f170
James Kirk Cropcho <JamesCropcho@users.noreply.github.com>
2022-12-04 19:51:14 -0500

    Make EsLint check itself

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,45 @@
 module.exports = {
-    "parser": "babel-eslint",
-    "env": {
-        "mongo": true,
-        "node": true,
-        "es6": true,
-        "mocha": true
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-      "indent": [
-          2,
-          2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "brace-style": [
-          2,
-          "1tbs",
-          { "allowSingleLine": true }
-        ]
-    },
-    "globals": {
-        "__quiet": false,
-        "slaveOk": false,
-        "collection": false,
-        "DBQuery": false,
-        "BinData": false,
-        "NumberLong": false,
-        "tojson": false
-
-    },
-    "parserOptions": {
-      "sourceType": "module"
-    }
+  'parser': 'babel-eslint',
+  'env': {
+    'mongo': true,
+    'node': true,
+    'es6': true,
+    'mocha': true
+  },
+  'extends': 'eslint:recommended',
+  'rules': {
+    'indent': [
+      2,
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'brace-style': [
+      2,
+      '1tbs',
+      { 'allowSingleLine': true }
+    ]
+  },
+  'globals': {
+    '__quiet': false,
+    'slaveOk': false,
+    'collection': false,
+    'DBQuery': false,
+    'BinData': false,
+    'NumberLong': false,
+    'tojson': false
+  },
+  'parserOptions': {
+    'sourceType': 'module'
+  }
 };
